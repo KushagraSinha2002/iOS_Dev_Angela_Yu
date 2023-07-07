561259031776bd35410fdaa2e809df8e5b153d3d
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2023-07-07 10:10:14 +0530

    I am Rich App

---
+++ Section 2/i am Rich/i am Rich/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  i am Rich
+//
+//  Created by Kushagra Sinha on 06/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
